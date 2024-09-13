bb3ee4873e44d05344aa02ae8778e67d89266be7
Harsh Vardhan <harshvardhan232002@gmail.com>
2024-09-13 16:52:34 +0530

    fourth

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { TbBrandWalmart } from "react-icons/tb";
-import { TiArrowSortedDown } from "react-icons/ti";
+import { FaChevronDown } from "react-icons/fa";
 import { IoSearchCircleSharp } from "react-icons/io5";
 import { GoHeart } from "react-icons/go";
 import { LuUser2 } from "react-icons/lu";
@@ -13,157 +13,167 @@ import { BsChevronDown } from "react-icons/bs";
 import { PiDownloadSimpleLight } from "react-icons/pi";
 import { PiGiftLight } from "react-icons/pi";
 import { CgNotes } from "react-icons/cg";
+import { IoChevronUp } from "react-icons/io5";
 
 
 import './Header.css'
 
 function Header() {
-  const [showcart,setshowcart]=useState(false);
-  const [showitem,setshowitem]=useState(false);
-  const [showaccount,setshowaccount]=useState(false);
+  const [showcart, setshowcart] = useState(false);
+  const [showitem, setshowitem] = useState(false);
+  const [showaccount, setshowaccount] = useState(false);
 
   return (
     <React.Fragment>
-        <header className='mainheader'>
-          <nav className='top-header'>
-            
-            <TbBrandWalmart color='#ffc220' size={36}/>
-      
-              <div className='select-bar' onClick={()=>setshowcart(!showcart)}>
-                <img src='./images/handcash.webp' alt='cash-hand'></img>
-                <div>
-                  <h5>How do you want your items?</h5>
-                  <p>Sacramento, 95829</p>
-                </div>
-                <TiArrowSortedDown/>
-              </div>
+      <header className='mainheader'>
+        <nav className='top-header'>
 
-              <div className='search-bar'>
-                <input type='search' name='search' placeholder='Search everything at Walmart online and in store'></input>
-                <IoSearchCircleSharp size={40} color='#004F9A'/>
-              </div>
+          <TbBrandWalmart color='#ffc220' size={36} />
 
-              <div className='item-bar' onClick={()=>setshowitem(!showitem)}>
-                <GoHeart color='white' size={17}/>
-                <div>
-                  <p>Reorder</p>
-                  <h5>My Items</h5>
-                </div>
-              </div>
+          <div className='select-bar' onClick={() => setshowcart(!showcart)}>
+            <img src='./images/handcash.webp' alt='cash-hand'></img>
+            <div>
+              <h5>How do you want your items?</h5>
+              <p>Sacramento, 95829</p>
+            </div>
+            {showcart ? <IoChevronUp /> : <FaChevronDown />}
+          </div>
 
-              <div className='sign-bar' onClick={()=>setshowaccount(!showaccount)}>
-                <LuUser2 color='white' size={17}/>
-                <div>
-                  <p>Sign In</p>
-                  <h5>Account</h5>
-                </div>
-              </div>
+          <div className='search-bar'>
+            <input type='search' name='search' placeholder='Search everything at Walmart online and in store'></input>
+            <IoSearchCircleSharp size={40} color='#004F9A' />
+          </div>
 
-              <div className='cart-bar'>
-                <BsCart color='white' size={20}/>
-                <div>
-                  <p>$0.00</p>
-                </div>
-              </div>
+          <div className='item-bar' onClick={() => setshowitem(!showitem)}>
+            <GoHeart color='white' size={17} />
+            <div>
+              <p>Reorder</p>
+              <h5>My Items</h5>
+            </div>
+          </div>
 
-          </nav>
+          <div className='sign-bar' onClick={() => setshowaccount(!showaccount)}>
+            <LuUser2 color='white' size={17} />
+            <div>
+              <p>Sign In</p>
+              <h5>Account</h5>
+            </div>
+          </div>
 
-          <nav className='bottom-header'>
-            <div className='first-name'>
-              <div className='first1'>
-              <RxDashboard/>
+          <div className='cart-bar'>
+            <BsCart color='white' size={20} />
+            <div>
+              <p>$0.00</p>
+            </div>
+          </div>
+
+        </nav>
+
+        <nav className='bottom-header'>
+          <div className='first-name'>
+            <div className='first1'>
+              <RxDashboard />
               <h5>Departments</h5>
-              <BsChevronDown/>
-              </div>
+              <BsChevronDown />
+            </div>
 
-              <div className='first2'>
-              <RxDashboard/>
+            <div className='first2'>
+              <RxDashboard />
               <h5>Services</h5>
-              <BsChevronDown/>
-              </div>
-              <div>|</div>
+              <BsChevronDown />
             </div>
-
-            <div className='second-name'>
-              <ul>
-                <li><a href='#'>Savings</a></li>
-                <li><a href='#'>Grocery & Essentials</a></li>
-                <li><a href='#'>New & Trending</a></li>
-                <li><a href='#'>Halloween</a></li>
-                <li><a href='#'>Beauty Glow Up</a></li>
-                <li><a href='#'>Electronics</a></li>
-                <li><a href='#'>Home</a></li>
-                <li><a href='#'>Fashion</a></li>
-                <li><a href='#'>Toy Shop</a></li>
-                <li><a href='#'>Registry</a></li>
-                <li><a href='#'>ONE Cash</a></li>
-                <li><a href='#'>Walmart+</a></li>
-              </ul>
-            </div>
-          </nav>
-        </header>
-
-
-
-        {showcart && <div className='address-cart'>
-          <div className='about-shipping'>
-            <div>
-            <img src='./images/truck.png' alt='Truck'></img>
-            <h5>Shipping</h5>
-            </div>
-            <div>
-            <img src='./images/car.png' alt='Truck'></img>
-            <h5>Pickup</h5>
-            </div>
-            <div>
-            <img src='./images/bag.png' alt='Truck'></img>
-            <h5>Delivery</h5>
-            </div>
+            <div>|</div>
           </div>
 
-          <div className='about-address'>
-            <div className='vehicles'>
-              <div><IoLocationOutline/></div>
-              <div>
-                <h5>Add an address for shipping and delivery</h5>
-                <p>Sacramento, CA 95829</p>
-              </div>
-            </div>
-            <div className='add-it'>Add address</div>
-          </div>
-
-          <div className='about-home'>
-            <div><IoHomeOutline/></div>
-            <div>
-            <h5>Sacramento Supercenter</h5>
-            <p>8915 GERBER ROAD, sacramento, CA 95829</p>
-            </div>
-            <div><MdArrowForwardIos/></div>
-          </div>
-        </div>}
-
-        {showitem && <div className='address-item'>
-          <div>
+          <div className='second-name'>
             <ul>
-              <li><a href='#'><PiDownloadSimpleLight/> Reorder</a></li>
-              <li><a href='#'><GoHeart/> Lists</a></li>
-              <li><a href='#'><PiGiftLight/> Registries</a></li>
+              <li><a href='#'>Savings</a></li>
+              <li><a href='#'>Grocery & Essentials</a></li>
+              <li><a href='#'>New & Trending</a></li>
+              <li><a href='#'>Halloween</a></li>
+              <li><a href='#'>Beauty Glow Up</a></li>
+              <li><a href='#'>Electronics</a></li>
+              <li><a href='#'>Home</a></li>
+              <li><a href='#'>Fashion</a></li>
+              <li><a href='#'>Toy Shop</a></li>
+              <li><a href='#'>Registry</a></li>
+              <li><a href='#'>ONE Cash</a></li>
+              <li><a href='#'>Walmart+</a></li>
             </ul>
           </div>
-        </div>}
+        </nav>
+      </header>
 
-        {showaccount && <div className='address-account'>
-          <div className='create-acc'><h5>Sign in or create account</h5></div>
-          <hr/>
-          <div className='history'>
-            <div><CgNotes/></div>
-            <div>Purchase History</div>
+
+
+      {showcart && <div className='outer-address' onClick={()=>setshowcart(false)}>
+      <div className='address-cart'>
+        <div className='about-shipping'>
+          <div>
+            <img src='./images/truck.png' alt='Truck'></img>
+            <h5>Shipping</h5>
           </div>
-          <div className='site'>
-            <div><TbBrandWalmart/></div>
-            <div>Walmart+</div>
+          <div>
+            <img src='./images/car.png' alt='Truck'></img>
+            <h5>Pickup</h5>
           </div>
-        </div>}
+          <div>
+            <img src='./images/bag.png' alt='Truck'></img>
+            <h5>Delivery</h5>
+          </div>
+        </div>
+      
+
+        <div className='about-address'>
+          <div className='vehicles'>
+            <div><IoLocationOutline /></div>
+            <div>
+              <h5>Add an address for shipping and delivery</h5>
+              <p>Sacramento, CA 95829</p>
+            </div>
+          </div>
+          <div className='add-it'>Add address</div>
+        </div>
+
+        <div className='about-home'>
+          <div><IoHomeOutline /></div>
+          <div>
+            <h5>Sacramento Supercenter</h5>
+            <p>8915 GERBER ROAD, sacramento, CA 95829</p>
+          </div>
+          <div><MdArrowForwardIos /></div>
+        </div>
+      </div>
+      </div>}
+
+      {showitem && <div className='out-item' onClick={()=>setshowitem(false)}>
+      <div className='address-item'>
+        <div>
+          <ul>
+            <li><a href='#'><PiDownloadSimpleLight /> Reorder</a></li>
+            <li><a href='#'><GoHeart /> Lists</a></li>
+            <li><a href='#'><PiGiftLight /> Registries</a></li>
+          </ul>
+        </div>
+      </div>
+      </div>}
+
+      {showaccount && <div className='oute-account' onClick={()=>setshowaccount(false)}>
+      <div className='address-account'>
+        <div className='create-acc'><h5>Sign in or create account</h5></div>
+        <hr />
+        <div className='history'>
+          <div><CgNotes /></div>
+          <div>Purchase History</div>
+        </div>
+        <div className='site'>
+          <div><TbBrandWalmart /></div>
+          <div>Walmart+</div>
+        </div>
+      </div>
+      </div>}
+
+
 
     </React.Fragment>
   )
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,104 @@
 import React from 'react'
+import './Home.css'
+
 
 function Home() {
   return (
     <>
-      
+      <main>
+        <section className='main-body'>
+          <div className='first'>
+            <div className='goat'>
+              <a href='#'>
+              <h2>The Top Toy List<br/>is here</h2>
+              <p>Shop now</p>
+              </a>
+            </div>
+            <div className='goat1'>
+              <img src='./images/goat.jpg' alt='cash-hand'></img>
+            </div>
+
+            <div className='chotibacchi'>
+              <a href='#'>
+              <h2>Only at Walmart-<br/>woohoo!</h2>
+              <p>Shop toys</p>
+              </a>
+              </div>
+              <div className='chotibacchi1'>
+              <img src='./images/chotibacchi.jpg' alt='cash-hand'></img>
+            </div>
+
+            <div className='dino'>
+            <a href='#'>
+              <h2>Big fun for ages 5-7</h2>
+              <p>Shop now</p>
+              </a>
+              <img src='./images/dino.jpg' alt='cash-hand'></img>
+            </div>
+          </div>
+
+          <div className='second'>
+          <div className='makeup'>
+              <a href='#'>
+              <p>Just in: premium beauty</p>
+              <h2>Pamper yourself</h2>
+              <p>Shop now</p>
+              </a>
+              <img src='./images/makeup.jpg' alt='cash-hand'></img>
+            </div>
+
+            <div className='insider-second'>
+            <div className='watch'>
+              <a href='#'>
+              <h2>Thinstant classic</h2>
+              <p>Preorder now</p>
+              </a>
+              <img src='./images/watch.jpg' alt='cash-hand'></img>
+            </div>
+            <div className='toycar'>
+              <a href='#'>
+              <h2>Toys & more up<br/>to 40% off</h2>
+              <p>shop now</p>
+              </a>
+              <img src='./images/toycar.jpg' alt='cash-hand'></img>
+            </div>
+            </div>
+
+            <div className='walmart+'>
+              <a href='#'>
+              <p>Walmart+ members save big with<br/>free delivery from stores</p>
+              <p>Terms apply</p>
+              <p>Join walmart+</p>
+              </a>
+              <img src='./images/walmart.webp' alt='cash-hand'></img>
+            </div>
+          </div>
+
+          <div className='third'>
+            <div className='nigga'>
+              <a href='#'>
+              <h2>Most-wanted for ages 12+</h2>
+              <p>shop now</p>
+              </a>
+              <img src='./images/nigga.jpg' alt='cash-hand'></img>
+            </div>
+            <div className='block'>
+              <a href='#'>
+              <h2>Play & grow for ages 1-2</h2>
+              <p>shop now</p>
+              </a>
+              <img src='./images/block.jpg' alt='cash-hand'></img>
+            </div>
+            <div className='notnigga'>
+              <a href='#'>
+              <h2>All smiles for ages<br/>2-4</h2>
+              <p>shop now</p>
+              </a>
+              <img src='./images/notnigga.jpg' alt='cash-hand'></img>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
